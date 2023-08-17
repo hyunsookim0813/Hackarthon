0210f9c51f8a5f879f348ffab033e7b7d38d1b60
hyunsookim0813 <100894661+hyunsookim0813@users.noreply.github.com>
2023-08-17 13:54:36 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let axios = require("axios");
 let app = express();
 let port = process.env.PORT || 80;
 
-app.use(express.static("index.html"));
+app.use(express.static("public_html"));
 app.listen(port, function(){
     console.log("HTML 서버 시작됨");
 });
